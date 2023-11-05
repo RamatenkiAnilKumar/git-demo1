0d47b4f6b951aaf48747d1d7abd3334ab52ac08f
Your Name <you@example.com>
2023-11-05 17:45:36 +0530

    my first commited

---
+++ src/main/java/com/app/MyTest.java
@@ -0,0 +1,18 @@
+package com.app;
+
+public class MyTest {
+
+	public void showMyName(String name) {
+		
+		
+			System.out.println("My NAme IS "+name);
+
+		}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
